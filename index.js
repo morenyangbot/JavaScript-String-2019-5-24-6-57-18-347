2ac8f3962d438ce4fd707d00ecc77a4f51aa9150
morenyangbot <45971234+morenyangbot@users.noreply.github.com>
2019-07-01 18:42:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,21 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
+console.log(name.toUpperCase());
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+var arr = sentence.split(' ');
+var arr1 = [];
+sentence[0] = sentence.substring(0, 1).toUpperCase();
+for (let i = 0; i < arr.length; i++) {
+    arr1.push(arr[i].substring(0,1).toUpperCase() + arr[i].substring(1));
+}
+console.log(arr1.join(' '))
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+console.log(money.replace(/￥/g, ''))
